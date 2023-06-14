ab6e69cdd85f8b0b6760270f1e7feb57df766a63
Daniele Campari <danielecampari@gmail.com>
2023-06-14 18:07:44 +0200

    add .divide(), .multiply() and their tests

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ export const Calculator = () => {
   const add = (a, b) => {
     if (checkInput(a) && checkInput(b)) {
       const c = a + b
-      return Number.isInteger(c) ? c : floatPrecision(a, b, c)
+      return Number.isInteger(c) ? c : floatPrecision(a, b, c, '+')
     }
 
     return 'Invalid input'
@@ -11,15 +11,29 @@ export const Calculator = () => {
   const subtract = (a, b) => {
     if (checkInput(a) && checkInput(b)) {
       const c = a - b
-      return Number.isInteger(c) ? c : floatPrecision(a, b, c)
+      return Number.isInteger(c) ? c : floatPrecision(a, b, c, '-')
     }
 
     return 'Invalid input'
   }
 
-  const divide = () => {}
+  const multiply = (a, b) => {
+    if (checkInput(a) && checkInput(b)) {
+      const c = a * b
+      return Number.isInteger(c) ? c : floatPrecision(a, b, c, '*')
+    }
 
-  const multiply = () => {}
+    return 'Invalid input'
+  }
+
+  const divide = (a, b) => {
+    if (checkInput(a) && checkInput(b)) {
+      if (b === 0) return 'Division for 0'
+      const c = a / b
+      return Number.isInteger(c) ? c : floatPrecision(a, b, c, '/')
+    }
+    return 'Invalid input'
+  }
 
   const checkInput = (input) => {
     if (isNaN(input)) return false
@@ -27,18 +41,29 @@ export const Calculator = () => {
     return false
   }
 
-  const floatPrecision = (x, y, z) =>
-    x.toString().split('.')[1].length > y.toString().split('.')[1].length
-      ? parseFloat(z.toFixed(Math.abs(x)))
-      : parseFloat(z.toFixed(Math.abs(y)))
+  const floatDigit = (x) =>
+    Number.isInteger(x) ? 0 : x.toString().split('.')[1].length
+
+  const floatPrecision = (x, y, z, sign) => {
+    if (sign === '+' || sign === '-') {
+      return floatDigit(x) > floatDigit(y)
+        ? parseFloat(z.toFixed(Math.abs(x)))
+        : parseFloat(z.toFixed(Math.abs(y)))
+    }
+
+    if (sign === '*') {
+      return parseFloat(z.toFixed(floatDigit(x) + floatDigit(y)))
+    }
+
+    if (sign === '/') {
+      return floatDigit(z) < 5 ? z : parseFloat(z.toFixed(5))
+    }
+  }
 
   return {
     add,
     subtract,
-    divide,
-    multiply
+    multiply,
+    divide
   }
 }
-
-const calc = Calculator()
-console.log(calc.add(NaN, 2))
--- calculator.test.js
+++ calculator.test.js
@@ -66,6 +66,14 @@ describe('method .add()', () => {
     expect(calc.add(3.4, 2.6)).toBe(6)
   })
 
+  it('does add an integer and a float', () => {
+    expect(calc.add(3, 6.4)).toBe(9.4)
+  })
+
+  it('does add a float and an integer', () => {
+    expect(calc.add(3.7, 2)).toBe(5.7)
+  })
+
   it('return a float sum of two positive float', () => {
     expect(calc.add(0.5, 2.6)).toBe(3.1)
   })
@@ -152,6 +160,14 @@ describe('method .subtract()', () => {
     expect(calc.subtract(3.4, 2.6)).toBe(0.8)
   })
 
+  it('does subtract an integer and a float', () => {
+    expect(calc.subtract(3, 6.4)).toBe(-3.4)
+  })
+
+  it('does subtract a float and an integer', () => {
+    expect(calc.subtract(3.7, 2)).toBe(1.7)
+  })
+
   it('return a float result of two positive float', () => {
     expect(calc.subtract(50.56, 2.6)).toBe(47.96)
   })
@@ -197,4 +213,162 @@ describe('method .subtract()', () => {
   })
 })
 
-//TODO: divide. multply
+describe('method .multiply()', () => {
+  it('does not accept string', () => {
+    expect(calc.multiply(1, '2')).toBe('Invalid input')
+  })
+
+  it('does not accept boolean', () => {
+    expect(calc.multiply(true, 2)).toBe('Invalid input')
+  })
+
+  it('does not accept objects', () => {
+    expect(calc.multiply(1, [1, 2])).toBe('Invalid input')
+  })
+
+  it('does not accept undefined', () => {
+    expect(calc.multiply(undefined, 2)).toBe('Invalid input')
+  })
+
+  it('does not accept NaN', () => {
+    expect(calc.multiply(NaN, 2)).toBe('Invalid input')
+  })
+
+  it('does not accept null', () => {
+    expect(calc.multiply(2, null)).toBe('Invalid input')
+  })
+
+  it('must have two input', () => {
+    expect(calc.multiply(1)).toBe('Invalid input')
+  })
+
+  it('does multiply two positive integer', () => {
+    expect(calc.multiply(3, 2)).toBe(6)
+  })
+
+  it('return a positive integer after multiply two positive integer', () => {
+    expect(calc.multiply(345, 212)).toBe(73140)
+  })
+
+  it('does multiply two positive float', () => {
+    expect(calc.multiply(3.4, 2.6)).toBe(8.84)
+  })
+
+  it('does multiply an integer and a float', () => {
+    expect(calc.multiply(3, 6.4)).toBe(19.2)
+  })
+
+  it('does multiply a float and an integer', () => {
+    expect(calc.multiply(3.7, -2)).toBe(-7.4)
+  })
+
+  it('return a float after multiply two positive float', () => {
+    expect(calc.multiply(50.56, 2.6)).toBe(131.456)
+  })
+
+  it('return a negative integer after multiply a positive and a negative integer', () => {
+    expect(calc.multiply(9, -4)).toBe(-36)
+  })
+
+  it('return a negative integer after multiply a negative and a positive integer', () => {
+    expect(calc.multiply(-4, 5)).toBe(-20)
+  })
+
+  it('return a positive integer after multiply two negative integer', () => {
+    expect(calc.multiply(-4, -5)).toBe(20)
+  })
+
+  it('return a negative float after multiply a positive and a negative float', () => {
+    expect(calc.multiply(9.5, -4.3)).toBe(-40.85)
+  })
+
+  it('return a negative float after multiply a negative and a positive float', () => {
+    expect(calc.multiply(-4.6, 5.9)).toBe(-27.14)
+  })
+
+  it('return a positive float after multiply two negative float', () => {
+    expect(calc.multiply(-4.4, -5.5)).toBe(24.2)
+  })
+})
+
+describe('method .divide()', () => {
+  it('does not accept string', () => {
+    expect(calc.divide(1, '2')).toBe('Invalid input')
+  })
+
+  it('does not accept boolean', () => {
+    expect(calc.divide(true, 2)).toBe('Invalid input')
+  })
+
+  it('does not accept objects', () => {
+    expect(calc.divide(1, [1, 2])).toBe('Invalid input')
+  })
+
+  it('does not accept undefined', () => {
+    expect(calc.divide(undefined, 2)).toBe('Invalid input')
+  })
+
+  it('does not accept NaN', () => {
+    expect(calc.divide(NaN, 2)).toBe('Invalid input')
+  })
+
+  it('does not accept null', () => {
+    expect(calc.divide(2, null)).toBe('Invalid input')
+  })
+
+  it('must have two input', () => {
+    expect(calc.divide(1)).toBe('Invalid input')
+  })
+
+  it('does not accept to divisor 0', () => {
+    expect(calc.divide(3, 0)).toBe('Division for 0')
+  })
+
+  it('does return 0 if dividend is 0', () => {
+    expect(calc.divide(0, 100)).toBe(0)
+  })
+
+  it('does add two positive integer', () => {
+    expect(calc.divide(3, 2)).toBe(1.5)
+  })
+
+  it('return a positive quotient after divide two positive integer', () => {
+    expect(calc.divide(345, 212)).toBe(1.62736)
+  })
+
+  it('does divide two positive float', () => {
+    expect(calc.divide(3.4, 2.6)).toBe(1.30769)
+  })
+
+  it('does divide an integer and a float', () => {
+    expect(calc.divide(3, 6.4)).toBe(0.46875)
+  })
+
+  it('does divide a float and an integer', () => {
+    expect(calc.divide(3.7, -2)).toBe(-1.85)
+  })
+
+  it('return a negative quotient after divide a positive and a negative integer', () => {
+    expect(calc.divide(9, -4)).toBe(-2.25)
+  })
+
+  it('return a negative quotient after divide a negative and a positive integer', () => {
+    expect(calc.divide(-4, 5)).toBe(-0.8)
+  })
+
+  it('return a positive quotient after divide two negative integer', () => {
+    expect(calc.divide(-4, -5)).toBe(0.8)
+  })
+
+  it('return a negative quotient after divide a positive and a negative float', () => {
+    expect(calc.divide(9.4, -4.3)).toBe(-2.18605)
+  })
+
+  it('return a negative quotient after divide a negative and a positive float', () => {
+    expect(calc.divide(-4.4, 5.3)).toBe(-0.83019)
+  })
+
+  it('return a positive quotient after divide two negative float', () => {
+    expect(calc.divide(-4.4, -5.4)).toBe(0.81481)
+  })
+})
