5c51e9b2c9585861974f509f7cadd8cbc994529d
Daniele Campari <danielecampari@gmail.com>
2023-06-16 19:02:29 +0200

    add encode to ceasar

--- ceasar.js
+++ ceasar.js
@@ -1,20 +1,42 @@
 export const CaesarCipher = () => {
   const encrypt = (str, fctr) => {
     if (checkType(str, 'string') && checkType(fctr, 'number')) {
+      if (isNotShifting(fctr)) fctr += 1
+      return cipher(str, fctr)
     }
     return 'Invalid input'
   }
 
   const checkType = (par, type) => {
-    if (isNaN(par)) return false
+    if (typeof par === 'number' && isNaN(par)) return false
     if (typeof par === type) return true
     return false
   }
 
-  const decrypt = () => {}
+  const isLetter = (char) => char.toLowerCase() !== char.toUpperCase()
+
+  const isNotShifting = (factor) => factor % 26 === 0
+
+  const cipher = (str, fact, encoded = '') => {
+    for (let char of str.trim()) {
+      let charCode = char.charCodeAt(0)
+
+      let shiftedCharCode =
+        charCode < 97
+          ? ((charCode + fact - 65) % 26) + 65
+          : ((charCode + fact - 97) % 26) + 97
+
+      let shiftedChar = isLetter(char)
+        ? String.fromCharCode(shiftedCharCode)
+        : char
+
+      encoded += shiftedChar
+    }
+
+    return encoded
+  }
 
   return {
-    encrypt,
-    decrypt
+    encrypt
   }
 }
--- ceasar.test.js
+++ ceasar.test.js
@@ -22,10 +22,6 @@ describe('Caesar CaesarCipher API', () => {
   it('has an .encrypt() method', () => {
     expect(cipher).toHaveProperty('encrypt')
   })
-
-  it('has a .decrypt() method', () => {
-    expect(cipher).toHaveProperty('decrypt')
-  })
 })
 
 describe('.encrypt() method', () => {
@@ -80,6 +76,60 @@ describe('.encrypt() method', () => {
   it('must have two parameter', () => {
     expect(cipher.encrypt('help')).toBe('Invalid input')
   })
+
+  it('use Cesar cipher and encrypt by 1', () => {
+    expect(cipher.encrypt('hello', 1)).toBe('ifmmp')
+  })
+
+  it('use Cesar cipher and encrypt by 2', () => {
+    expect(cipher.encrypt('hello', 2)).toBe('jgnnq')
+  })
+
+  it('does not change punctuation', () => {
+    expect(cipher.encrypt('!@#$%^&*()-_=+[]{};:,<.>/?', 1)).toBe(
+      '!@#$%^&*()-_=+[]{};:,<.>/?'
+    )
+  })
+
+  it('does not change spaces', () => {
+    expect(cipher.encrypt('hello, world!', 1)).toBe('ifmmp, xpsme!')
+  })
+
+  it('is case sensitive: all upper cases', () => {
+    expect(cipher.encrypt('HELLO, WORLD!', 1)).toBe('IFMMP, XPSME!')
+  })
+
+  it('is case sensitive: upper and lower cases', () => {
+    expect(cipher.encrypt('hElLO, woRlD!', 1)).toBe('iFmMP, xpSmE!')
+  })
+
+  it('trim white spaces at the beginning and at the end', () => {
+    expect(cipher.encrypt('        hello, world!           ', 1)).toBe(
+      'ifmmp, xpsme!'
+    )
+  })
+
+  it('does wrap if char is z', () => {
+    expect(cipher.encrypt('z', 1)).toBe('a')
+  })
+
+  it('does wrap if char is Z', () => {
+    expect(cipher.encrypt('Z', 1)).toBe('A')
+  })
+
+  it('does wrap at char z if shift code is > di 26 ', () => {
+    expect(cipher.encrypt('zzzz', 27)).toBe('aaaa')
+  })
+
+  it('does wrap at char Z if shift code is > di 26 ', () => {
+    expect(cipher.encrypt('ZZZZ', 27)).toBe('AAAA')
+  })
+
+  it('does encrypt multiple of 26 by increasing shift factor by 1', () => {
+    expect(cipher.encrypt('hello', 26)).toBe('ifmmp')
+  })
+
+  it('does encrypt ff shift factor is 0 by increasing shift factor by 1', () => {
+    expect(cipher.encrypt('hello', 0)).toBe('ifmmp')
+  })
 })
-// Test trim, test case insensitive, test punctuation and test vrapping from z
-// to a.
