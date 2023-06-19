ac1ae9576e24812fb55f907b66ba9ba44a1fa66e
Daniele Campari <danielecampari@gmail.com>
2023-06-19 16:54:45 +0200

    add comments to capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,8 @@
+/**
+ * Capitalize string
+ * @param {String} str
+ * @returns
+ */
 export const capitalize = (str) => {
   if (typeof str !== 'string') return 'Input must be string'
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`
