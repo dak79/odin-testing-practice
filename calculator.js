2f852b48b33a9a4ac89467a39f350fc06c5eb116
Daniele Campari <danielecampari@gmail.com>
2023-06-19 16:53:27 +0200

    add comment to calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,10 @@
 export const Calculator = () => {
+  /**
+   * Sum two numbers
+   * @param {Number} a
+   * @param {Number} b
+   * @returns {Number}
+   */
   const add = (a, b) => {
     if (checkInput(a) && checkInput(b)) {
       const c = a + b
@@ -8,6 +14,12 @@ export const Calculator = () => {
     return 'Invalid input'
   }
 
+  /**
+   * Subtract two numbers
+   * @param {Number} a
+   * @param {Number} b
+   * @returns {Number}
+   */
   const subtract = (a, b) => {
     if (checkInput(a) && checkInput(b)) {
       const c = a - b
@@ -17,6 +29,12 @@ export const Calculator = () => {
     return 'Invalid input'
   }
 
+  /**
+   * Multiply two numbers
+   * @param {Number} a
+   * @param {Number} b
+   * @returns {Number}
+   */
   const multiply = (a, b) => {
     if (checkInput(a) && checkInput(b)) {
       const c = a * b
@@ -26,6 +44,12 @@ export const Calculator = () => {
     return 'Invalid input'
   }
 
+  /**
+   * Divide two numbers
+   * @param {Number} a
+   * @param {Number} b
+   * @returns {Number}
+   */
   const divide = (a, b) => {
     if (checkInput(a) && checkInput(b)) {
       if (b === 0) return 'Division for 0'
@@ -35,15 +59,33 @@ export const Calculator = () => {
     return 'Invalid input'
   }
 
+  /**
+   * Check if the input is valid aka typeof number.
+   * @param {} input
+   * @returns
+   */
   const checkInput = (input) => {
     if (isNaN(input)) return false
     if (typeof input === 'number') return true
     return false
   }
 
+  /**
+   * Count float digit
+   * @param {Number} x
+   * @returns {Number}
+   */
   const floatDigit = (x) =>
     Number.isInteger(x) ? 0 : x.toString().split('.')[1].length
 
+  /**
+   * Define float precision
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} z
+   * @param {'+'|'-'|'*'|'/'|} sign
+   * @returns
+   */
   const floatPrecision = (x, y, z, sign) => {
     if (sign === '+' || sign === '-') {
       return floatDigit(x) > floatDigit(y)
