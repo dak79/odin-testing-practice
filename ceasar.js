985d92e7f5fe899f52fae6106e28cf44fb8ecd5a
Daniele Campari <danielecampari@gmail.com>
2023-06-19 17:00:21 +0200

    add comments to ceasar.js

--- ceasar.js
+++ ceasar.js
@@ -1,4 +1,10 @@
 export const CaesarCipher = () => {
+  /**
+   * Encryp a string with Ceasar Cipher by a shift factor
+   * @param {String} str - String to encrypt
+   * @param {Number} fctr - Shift factor
+   * @returns
+   */
   const encrypt = (str, fctr) => {
     if (checkType(str, 'string') && checkType(fctr, 'number')) {
       if (isNotShifting(fctr)) fctr += 1
@@ -7,16 +13,38 @@ export const CaesarCipher = () => {
     return 'Invalid input'
   }
 
+  /**
+   * Check if arguments are in the corrct input type
+   * @param {} par
+   * @param {'number'|'string'} type
+   * @returns
+   */
   const checkType = (par, type) => {
     if (typeof par === 'number' && isNaN(par)) return false
     if (typeof par === type) return true
     return false
   }
 
+  /**
+   * Check if the char is a letter
+   * @param {String} char
+   * @returns {Boolean}
+   */
   const isLetter = (char) => char.toLowerCase() !== char.toUpperCase()
 
+  /**
+   * Check if shifting factor does not work
+   * @param {Number} factor
+   * @returns
+   */
   const isNotShifting = (factor) => factor % 26 === 0
-
+  /**
+   * Return the string encoded
+   * @param {String} str
+   * @param {Number} fact
+   * @param {String} encoded
+   * @returns
+   */
   const cipher = (str, fact, encoded = '') => {
     for (let char of str.trim()) {
       let charCode = char.charCodeAt(0)
