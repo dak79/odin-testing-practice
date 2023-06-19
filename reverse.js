5d620be4641bb708b2ef9a707bedf3bca7c7dcd1
Daniele Campari <danielecampari@gmail.com>
2023-06-19 17:01:58 +0200

    add comments to reverse.js

--- reverse.js
+++ reverse.js
@@ -1,3 +1,8 @@
+/**
+ * Reverse a string
+ * @param {String} str
+ * @returns
+ */
 export const reverse = (str) => {
   if (typeof str !== 'string') return 'Input must be string'
   return str.split('').reverse().join('')
